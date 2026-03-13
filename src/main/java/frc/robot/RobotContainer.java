93d759e12250bd79447b00fd15b75a08ba2d8efb
Abbbb ffffff <sabfems@gmail.com>
2026-03-12 21:44:57 -0600

    Fix template

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,22 +1,23 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.subsystems.Chassis;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.*;
 public class RobotContainer {
     
   public Chassis m_chassis = new Chassis();
   public Shooter m_shooter = new Shooter();
   public Intake m_intake = new Intake();
-  public Climber m_climber = new Climber();
+  public climber m_climber = new climber();
+
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
 
 }
